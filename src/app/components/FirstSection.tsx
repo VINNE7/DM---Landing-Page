234138b9115360ceec4cf5d3bb9a306d6858acf4
VINNE7 <marcusv.ribas7@gmail.com>
2023-08-20 19:14:46 -0300

    feat: add image, make first section texts

--- src/app/components/FirstSection.tsx
+++ src/app/components/FirstSection.tsx
@@ -1,5 +1,20 @@
+import Image from "next/image";
+import demolayLogo from "../../../public/demolayLogo.png";
+
 const FirstSection = () => {
-  return <h1>hello</h1>;
+  return (
+    <div className="flex w-[600px] h-[80vh] m-auto">
+      <Image className="" src={demolayLogo} alt="demolay logo" />
+      <div className="">
+        <h2 className="text-4xl">Trabalhando com a juventude desde 1919</h2>
+        <p>
+          A Ordem DeMolay é uma instituição fraternal, para jovens rapazes de 12
+          a 21 anos, com o objetivo de melhorar a sociedade através de
+          protagonismo juvenil e de preparação cívica e social.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default FirstSection;
